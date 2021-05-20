969298d28bde62afdb111c9d34c0db66d9af032a
saif ahmed <asshaik@corp.untd.com>
2021-05-20 23:52:20 +0530

    Add Connect_4 game

---
+++ Connect 4 goldTest/Connect_4_goldTest.m
@@ -0,0 +1,37 @@
+//
+//  Connect_4_goldTest.m
+//  Connect 4 goldTest
+//
+//  Created by Shaik A S on 28/01/19.
+//  Copyright © 2019 SHAIK AS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Connect_4_goldTest : XCTestCase
+
+@end
+
+@implementation Connect_4_goldTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
